393d3a5412c23585d31d70ef2ba2d4c38d2361ab
DeKarLo <karen.ananyan2017@mail.ru>
2024-02-19 23:28:46 +0600

    repository done

--- services/contact/cmd/internal/repository/repository.go
+++ services/contact/cmd/internal/repository/repository.go
@@ -14,3 +14,43 @@ type GroupRepository interface {
 	GetGroupByID(id int) (*domain.Group, error)
 	AddContactToGroup(contactID, groupID int) error
 }
+
+type contactRepository struct{}
+
+func (r *contactRepository) CreateContact(contact *domain.Contact) error {
+	return nil
+}
+
+func (r *contactRepository) GetContactByID(id int) (*domain.Contact, error) {
+	return nil, nil
+}
+
+func (r *contactRepository) UpdateContact(contact *domain.Contact) error {
+	return nil
+}
+
+func (r *contactRepository) DeleteContact(id int) error {
+	return nil
+}
+
+type groupRepository struct{}
+
+func (r *groupRepository) CreateGroup(group *domain.Group) error {
+	return nil
+}
+
+func (r *groupRepository) GetGroupByID(id int) (*domain.Group, error) {
+	return nil, nil
+}
+
+func (r *groupRepository) AddContactToGroup(contactID, groupID int) error {
+	return nil
+}
+
+func NewContactRepository() ContactRepository {
+	return &contactRepository{}
+}
+
+func NewGroupRepository() GroupRepository {
+	return &groupRepository{}
+}
